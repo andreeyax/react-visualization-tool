1b5f6be5c91cf2824b6d47166c7c7026c7bea41d
andreeyax <andrija.vt.tomic@gmail.com>
2025-10-30 22:59:58 +0100

    homepage design changes

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Home(){
     
 
     return(
-        <div className="App">
+        <div className="Apphome">
     
             <div className="welcome">
                 Please choose option from the menu!
